4fde6cdf700595c8675b15bd4d9698eb0192f05f
andreainno22 <andrea.innocenti12@stud.unifi.it>
2025-07-20 14:48:01 +0200

    notebook finito

--- .ipynb_checkpoints/utils-checkpoint.py
+++
@@ -1,8 +0,0 @@
-from costants import SHOT_LENGTH
-
-
-# Funzione per filtrare e tagliare i colpi a SHOT_LENGTH
-def filter_and_trim_shots(shots_df):
-    shots_df = shots_df[shots_df['shots'].apply(len) >= SHOT_LENGTH]
-    shots_df["shots"] = shots_df['shots'].apply(lambda x: x[:SHOT_LENGTH])
-    return shots_df
